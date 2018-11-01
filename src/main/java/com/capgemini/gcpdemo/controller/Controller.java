67b6e52816fde3d69fbf2ad45c1ede440d8a475a
Jayanth Vuddagiri <vzv0012@tigermail.auburn.edu>
2018-10-31 20:26:20 -0400

    disabled security

--- src/main/java/com/capgemini/gcpdemo/controller/Controller.java
+++ src/main/java/com/capgemini/gcpdemo/controller/Controller.java
@@ -1,15 +1,18 @@
 package com.capgemini.gcpdemo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+	 @CrossOrigin
 	 @RequestMapping("/")
 	  public String home() {
 	    return "Hello World!";
 	  }
 	 
+	  @CrossOrigin
 	  @RequestMapping("/_ah/health")
 	  public String healthy() {
 	    // Message body required though ignored
